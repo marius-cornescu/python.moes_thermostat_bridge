c45871a218b011ffa3621767c008ce02555fe054
Marius Cornescu <marius_cornescu@yahoo.com>
2025-12-12 19:37:55 +0100

    Adhere more to tinytuya design / eliminate some errors of mapping

--- moes/MoesThermostat.py
+++ moes/MoesThermostat.py
@@ -18,7 +18,7 @@ from bridge import TuyaCallbackOnAction
 ##########################################################################################################
 
 # 'dps': {'1': True, '2': 40, '3': 40, '4': '0', '5': False, '6': False, '102': 0, '104': True}
-#       {'1': True, '2': 46, '3': 41, '4': '0', '5': False, '6': False, '102': 0, '104': True}
+#        {'1': True, '2': 46, '3': 41, '4': '0', '5': False, '6': False, '102': 0, '104': True}
 # '1': True     = Power mode = ON / OFF
 # '2': 40,      = Temperature =
 #           60 = 30 grade
@@ -36,25 +36,7 @@ from bridge import TuyaCallbackOnAction
 
 ITERATION_INDEX_LIMIT = 2000
 
-MOES_POWER_STATUS = 1
-MOES_TARGET_TEMPERATURE = 2
-MOES_MEASURED_TEMPERATURE = 3
-MOES_OPERATING_MODE = 4
-MOES_ECO_MODE_ENABLED = 5
-MOES_LOCK_ENABLED = 6
-
 MOES_TEMPERATURE_SCALE = 2
-
-MOES_METRIC_MAP = {
-    MOES_POWER_STATUS: 'is_on',
-    MOES_TARGET_TEMPERATURE: 'target_temperature',
-    MOES_MEASURED_TEMPERATURE: 'home_temperature',
-    MOES_OPERATING_MODE: 'manual_operating_mode',
-    MOES_ECO_MODE_ENABLED: 'eco_mode',
-    MOES_LOCK_ENABLED: 'lock_enabled',
-    102: None,
-    104: None,
-}
 
 ##########################################################################################################
 @dataclass
@@ -79,6 +61,59 @@ class ThermostatState(object):
 
 ##########################################################################################################
 
+class MoesBht002Thermostat(Contrib.ThermostatDevice):
+    __POWER_STATUS = 1
+    __TARGET_TEMPERATURE = 2
+    __MEASURED_TEMPERATURE = 3
+    __OPERATING_MODE = 4
+    __ECO_MODE_ENABLED = 5
+    __LOCK_ENABLED = 6
+
+    __IGNORE_METRIC1 = 102
+    __IGNORE_METRIC2 = 104
+
+    moes_metric_map = {
+        __POWER_STATUS:         'is_on',
+        __TARGET_TEMPERATURE:   'target_temperature',
+        __MEASURED_TEMPERATURE: 'home_temperature',
+        __OPERATING_MODE:       'manual_operating_mode',
+        __ECO_MODE_ENABLED:     'eco_mode',
+        __LOCK_ENABLED:         'lock_enabled',
+        __IGNORE_METRIC1:       None,
+        __IGNORE_METRIC2:       None,
+    }
+
+    dps_data = {
+        f'{__TARGET_TEMPERATURE}':   {'name': 'target_temperature', 'alt': 'setpoint_c', 'scale': 2},
+        f'{__MEASURED_TEMPERATURE}': {'name': 'home_temperature', 'alt': 'temperature_c', 'scale': 2},
+        f'{__OPERATING_MODE}':       {'name': 'manual_operating_mode', 'enum': ['0','1']},
+        f'{__ECO_MODE_ENABLED}':     {'name': 'eco_mode', 'decode': bool},
+        f'{__LOCK_ENABLED}':         {'name': 'lock_enabled', 'decode': bool},
+        f'{__IGNORE_METRIC1}':       {'name': 'ignore_1'},
+        f'{__IGNORE_METRIC2}':       {'name': 'ignore_2'}
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(MoesBht002Thermostat, self).__init__(*args, **kwargs)
+
+    def map_dps_metric_to_state(self, dps_metric_id: str) -> str | None:
+        try:
+            return self.moes_metric_map.get(int(dps_metric_id), None)
+        except Exception as e:
+            logging.getLogger(__name__).error(f'Exception while _map_dps_metric_to_state [{dps_metric_id}]: [%s]', e)
+            if logging.getLogger(__name__).isEnabledFor(logging.ERROR):
+                traceback.print_exc()
+
+        return None
+
+    def map_state_to_dps_metric(self, state_field: str) -> int | None:
+        for key, value in self.moes_metric_map.items():
+            if value == state_field:
+                return key
+        return None
+
+##########################################################################################################
+
 @dataclass
 class MoesBhtThermostat(object):
     name: str
@@ -86,7 +121,7 @@ class MoesBhtThermostat(object):
     local_ip: str
     tuya_local_key: str
 
-    device: Final[Contrib.ThermostatDevice]
+    device: Final[MoesBht002Thermostat]
 
     state_current: ThermostatState
     state_previous: ThermostatState
@@ -105,14 +140,14 @@ class MoesBhtThermostat(object):
     full_status_publish_time: Optional[float] = None
 
     def __init__(self, name: str, tuya_id: str, local_ip: str, tuya_local_key: str,
-                 device: Contrib.ThermostatDevice | None = None):
+                 device: MoesBht002Thermostat | None = None):
         self.name = name
         self.tuya_id = tuya_id
         self.local_ip = local_ip
         self.tuya_local_key = tuya_local_key
 
         if device is None:
-            device = Contrib.ThermostatDevice(tuya_id, local_ip, tuya_local_key, version=3.3)
+            device = MoesBht002Thermostat(tuya_id, local_ip, tuya_local_key, version=3.3)
         self.device = device
 
         self.state_current = ThermostatState(
@@ -172,8 +207,7 @@ class MoesBhtThermostat(object):
                 self.ping_time = self._next_ping_time()
 
             data = self._get_data()
-            if data:
-                #self.__print_data(data)
+            if data and 'Error' not in data:
                 had_state_updates = self._process_raw_data_updates(data)
 
                 if had_state_updates and not self.is_synchronized:
@@ -220,7 +254,7 @@ class MoesBhtThermostat(object):
         dps_data = try_get_from_structure(data, ['dps'])
 
         if dps_data is not None:
-            state_data = dict_map_keys(dps_data, self.__map_dps_metric_to_state)
+            state_data = dict_map_keys(dps_data, self.device.map_dps_metric_to_state)
             had_state_updates = self._process_data_updates(state_data)
         else:
             logging.getLogger(__name__).debug('No DPS data available')
@@ -252,17 +286,6 @@ class MoesBhtThermostat(object):
                 self._handle_on_state_changed()
 
         return had_state_updates
-
-    @staticmethod
-    def __map_dps_metric_to_state(dps_metric_id: str) -> str | None:
-        try:
-            return MOES_METRIC_MAP.get(int(dps_metric_id), None)
-        except Exception as e:
-            logging.getLogger(__name__).error(f'Exception while _map_dps_metric_to_state [{dps_metric_id}]: [%s]', e)
-            if logging.getLogger(__name__).isEnabledFor(logging.ERROR):
-                traceback.print_exc()
-
-        return None
 
     def __process_data_update(self, state_field: str, metric_value: str) -> bool:
         logging.getLogger(__name__).debug(f'Processing update for [{self.name}] to metric=[{state_field}] value=[{metric_value}]')
@@ -381,7 +404,7 @@ class MoesBhtThermostat(object):
         moes_temp = int(temperature * MOES_TEMPERATURE_SCALE)
 
         logging.getLogger(__name__).debug(f"setMoesTemperature({moes_temp})")
-        self.device.set_value(MOES_TARGET_TEMPERATURE, moes_temp)
+        self.device.set_value(index=self.device.map_state_to_dps_metric('target_temperature'), value=moes_temp)
 
         self.state_previous = self.state_current.clone()
         self.state_current.target_temperature = temperature
@@ -391,7 +414,7 @@ class MoesBhtThermostat(object):
         logging.getLogger(__name__).info(f"Setting [{self.name}] operating mode [{'MANUAL' if enabled else 'AUTO'}]")
 
         moes_op_mode_value = '1' if enabled else '0'
-        self.device.set_value(index=MOES_OPERATING_MODE, value=moes_op_mode_value, nowait=True)
+        self.device.set_value(index=self.device.map_state_to_dps_metric('manual_operating_mode'), value=moes_op_mode_value, nowait=True)
 
         self.state_previous = self.state_current.clone()
         self.state_current.manual_operating_mode = enabled
@@ -399,7 +422,7 @@ class MoesBhtThermostat(object):
 
     def set_eco_mode(self, eco_mode: bool):
         logging.getLogger(__name__).info(f"Setting [{self.name}] eco mode [{'ON' if eco_mode else 'OFF'}]")
-        self.device.set_value(MOES_ECO_MODE_ENABLED, eco_mode)
+        self.device.set_value(index=self.device.map_state_to_dps_metric('eco_mode'), value=eco_mode)
 
         self.state_previous = self.state_current.clone()
         self.state_current.eco_mode = eco_mode
@@ -407,13 +430,8 @@ class MoesBhtThermostat(object):
 
     def set_lock_enabled(self, lock_enabled: bool):
         logging.getLogger(__name__).info(f"Setting [{self.name}] lock mode [{'ON' if lock_enabled else 'OFF'}]")
-        self.device.set_value(MOES_LOCK_ENABLED, lock_enabled)
+        self.device.set_value(index=self.device.map_state_to_dps_metric('lock_enabled'), value=lock_enabled)
 
         self.state_previous = self.state_current.clone()
         self.state_current.lock_enabled = lock_enabled
         logging.getLogger(__name__).info(f"Set [{self.name}] [lock_enabled] to [{self.state_current.lock_enabled}]")
-
-    @staticmethod
-    def __print_data(data):
-        if 'dps' in data and len(data['dps']) > 0:
-            logging.getLogger(__name__).info(f'>>>> RAW DPS: [{data["dps"]}]')
